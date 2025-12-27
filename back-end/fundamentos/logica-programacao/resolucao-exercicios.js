4ea43fa67678d199de157885574da357915c0561
William Brito Dev <contato.williambritodev@hotmail.com>
2025-12-27 09:25:57 -0300

    Arquivo com a resolução de exercícios

---
+++ back-end/fundamentos/logica-programacao/resolucao-exercicios.js
@@ -0,0 +1,16 @@
+// 1 - Faça um algoritmo que leia os valores de A, B, C e em seguida imprima na tela a soma entre A e B é mostre se a soma é menor que C.
+
+const A = console.log("Digite o valor de A: ")
+const B = console.log("Digite o valor de B: ")
+const C = console.log("Digite o valor de C: ")
+
+const soma = A + B 
+
+if (soma < C) {
+    console.log(`A soma entre ${A} e ${B} é menor do ${C} `)
+} else {
+    console.log(`A soma entre ${A} e ${B} é maior do que ${C} `)
+}
+
+
+
